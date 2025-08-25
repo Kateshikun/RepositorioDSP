a8d13763afb28d1ec4d0df8b67cd227c64acb4d4
Kateshikun <danieladriancastillogarcia@gmail.com>
2025-08-24 19:38:17 -0600

    Main
    Codigo hecho por adrian

---
+++ Ejercicio2_ DSP/Conjunto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2__DSP
+{
+    internal class Conjunto
+    {
+        //Creacion de la lista donde se almacenaran numeros decimales positivos
+        //Ya esta declarada como privada, por lo tanto solo se podra acceder a esta desde una instancia de la clase
+        private List<decimal> conjuntoNumeros = new List<decimal>();
+        private int cantidadNumeros = 0;
+
+
+        //Metodo constructor vacio que inicializa la lista con tamaño predeterminado si no se dan parametros
+        public Conjunto()
+        {
+            conjuntoNumeros = new List<decimal>();
+        }
+
+        //Sobrecarga de metodo constructor que recibe un vector de numeros decimales y los agrega a la lista
+        public Conjunto(decimal[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                this.conjuntoNumeros.Add(numeros[i]);
+                cantidadNumeros++;
+            }
+        }
+
+        //Metodo que retorna la cantidad de numeros en la lista
+        public string RetornarCantidad()
+        {
+            return "La cantidad de numeros en el conjunto es: " + cantidadNumeros;
+        } 
+
+        /*public  List<decimal> getConjuntoNumeros() //No se si hay que poner este metodo aun hmmmmmmm
+        {
+            return this.conjuntoNumeros;
+        }*/
+
+
+        //Metodo que comprueba que un numero decimal positivo este en la lista. Retorna true si esta y false si no.
+        public bool ComprobarExistencia(decimal numero)
+        {
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                if (this.conjuntoNumeros[i] == numero)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        //Metodo que inserta un numero decimal positivo en la lista. Retorna true si se pudo insertar y false si no.
+        public bool InsertarNumero(decimal numero)
+        {
+            if (numero < 0)
+            {
+                return false;
+            }
+
+            else for (int i = 0; i < cantidadNumeros; i ++) {
+                if (this.conjuntoNumeros[i] == numero) {
+                return false;
+                }
+            } 
+
+            this.conjuntoNumeros.Add(numero);
+            cantidadNumeros++;
+            return true;
+        }
+    }
+}
---
+++ Ejercicio2_ DSP/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2__DSP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion de instancia de la clase Conjunto
+            Conjunto conjunto1 = new Conjunto();
+            Conjunto conjunto2 = new Conjunto();
+            Conjunto conjunto3 = new Conjunto(ValoresAleatorios());
+
+            //Pedir datos al usuario
+            Console.WriteLine("Bienvenido al programa de Conjunto de Elementos");
+            Console.ReadKey();
+            Console.Write("\n--A continuacion digite 5 decimales positivos que desea agregar al conjunto--"); //No se si peuden convertir esta parte en metodo Rudy o fran jejejeje :v
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"\nnum {i + 1}: ");
+                if (decimal.TryParse(Console.ReadLine(), out decimal num))
+            {
+                if (conjunto1.InsertarNumero(num))
+                {
+                    Console.WriteLine("El numero se ha insertado correctamente");
+                }
+                else
+                {
+                    Console.WriteLine("El numero no se ha podido insertar");
+                }
+                conjunto1.InsertarNumero(num);
+            }
+                else
+                {
+                    Console.WriteLine("El valor ingresado no es un decimal positivo. Intente de nuevo.");
+                }
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Los numeros aleatorios generados para el conjunto 3 son: ");
+            for (int i = 0; i < 15; i++)
+            {
+               // Console.WriteLine(conjunto3.); Aqui continuen cuando hagan el metodo F de la guia
+            }
+
+        }
+
+        public static decimal[] ValoresAleatorios()
+        {
+            //Obtendremos 15 numeros decimales aleatorios, cada uno en el rango [-10, 10]
+            Random aleatorio = new Random();
+            decimal[] numAleatorios = new decimal[15];
+            
+            for (int i = 0; i < 15; i++)
+            {              
+               double numero = Math.Round((aleatorio.NextDouble() * 20) -10 ,2);
+               numAleatorios[i] = (decimal)numero;
+            }         
+            return numAleatorios;
+        }
+    }
+}
